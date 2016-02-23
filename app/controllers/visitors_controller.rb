a8b5d5db9a5b131ac2c096db42cba182acd79b84
Joshua Echeverri <josheche@gmail.com>
2016-02-23 17:15:35 -0500

    dotenv

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,9 @@
 class VisitorsController < ApplicationController
+
+  def index
+    require 'coinbase/wallet'
+    client = Coinbase::Wallet::Client.new(api_key: <api key>, api_secret: <api secret>)
+    price = client.spot_price({currency: 'USD'})
+  end
+
 end
