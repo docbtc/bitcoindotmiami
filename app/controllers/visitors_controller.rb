16ffc3707a3ca098cb926342472fd04f8c989bfa
Joshua Echeverri <josheche@gmail.com>
2016-02-23 17:48:49 -0500

    landing box

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,7 @@
 class VisitorsController < ApplicationController
 
   def index
-    require 'coinbase/wallet'
-    client = Coinbase::Wallet::Client.new(api_key: <api key>, api_secret: <api secret>)
-    price = client.spot_price({currency: 'USD'})
+    
   end
 
 end
