3423d45610ae1c4adb50f069d7ff95bc7df13d55
Joshua Echeverri <josheche@gmail.com>
2016-02-29 15:27:48 -0500

    coinbase price to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :init
+
+  def init
+    @coinbase_price = coinbase_price
+  end
+
+  def coinbase_price
+    begin
+      response = JSON.parse(HTTParty.get("https://api.coinbase.com/v2/prices/spot").body)
+      @cb_price = response["data"]["amount"]
+      return @cb_price
+    rescue
+      return 0
+    end
+  end
 end
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,17 +1,6 @@
 class VisitorsController < ApplicationController
 
   def index
-    @coinbase_price = coinbase_price
-  end
-
-  def coinbase_price
-    begin
-      response = JSON.parse(HTTParty.get("https://api.coinbase.com/v2/prices/spot").body)
-      @cb_price = response["data"]["amount"]
-      return @cb_price
-    rescue
-      return 0
-    end
   end
 
 end
