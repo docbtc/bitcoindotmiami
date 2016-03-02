6d78e7408f2b43ec43de7cf17dc8809659817d72
Joshua Echeverri <josheche@gmail.com>
2016-03-01 19:43:51 -0500

    bitcoin price ticker edits, landing, fonts added, compiling

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Bitcoindotmiami
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Allows fonts in asset pipeline. To remove just comment out and delete fonts folder in assets.
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
