67a5268a8952b298d987105c10b1ec60a080cb1d
Joshua Echeverri <josheche@gmail.com>
2016-03-08 17:39:09 -0500

    meetup api up and spitting payloads everywhere

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   def init
     @coinbase_price = coinbase_price
+    @meetup_event = meetup_event
   end
 
   def coinbase_price
@@ -18,4 +19,18 @@ class ApplicationController < ActionController::Base
       return 0
     end
   end
+
+  def meetup_event
+    bitcoin_meetup = HTTParty.get "https://api.meetup.com/2/events?&sign=true&photo-host=public&fields=self&group_urlname=Bitcoin-of-South-Florida&page=20"
+    results_set = []
+    bitcoin_meetup["results"].each do |result|
+      targs = result["description"].split("<img src=\"")
+      image = targs[1].split("\" />")
+      result = { name: result["name"], location: "#{result['venue']['name']}, #{result['venue']['address_1']}, #{result['venue']['city']}", image: "#{image[0]}" }
+      results_set.push(result)
+    end
+
+    return results_set
+  end
+
 end
