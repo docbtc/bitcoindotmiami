edde654474f3a66474a1f0490993b670edd57578
Joshua Echeverri <josheche@gmail.com>
2016-03-22 16:44:55 -0400

    meetup api fix, added refresh miami, and code for miami

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def init
     @coinbase_price = coinbase_price
-    # @meetup_event = meetup_event
+    @meetup_event = meetup_event
   end
 
   def coinbase_price
@@ -20,17 +20,33 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # def meetup_event
-  #   bitcoin_meetup = HTTParty.get "https://api.meetup.com/2/events?&sign=true&photo-host=public&fields=self&group_urlname=Bitcoin-of-South-Florida&page=20"
-  #   results_set = []
-  #   bitcoin_meetup["results"].each do |result|
-  #     targs = result["description"].split("<img src=\"")
-  #     image = targs[1].split("\" />")
-  #     result = { name: result["name"], location: "#{result['venue']['name']}, #{result['venue']['address_1']}, #{result['venue']['city']}", image: "#{image[0]}" }
-  #     results_set.push(result)
-  #   end
-  #
-  #   return results_set
-  # end
+  def meetup_event
+    results_set = []
+    bitcoin_meetup = HTTParty.get "https://api.meetup.com/2/events?&sign=true&photo-host=public&fields=self&group_urlname=Bitcoin-of-South-Florida&page=20"
+    bitcoin_meetup["results"].each do |result|
+      targs = result["description"].split("<img src=\"")
+      image = targs[1].split("\" />") rescue ["http://www.andybarratt.co.uk/wp-content/uploads/2013/08/Bitcoin-Coin-1024.jpg"]
+      result = { name: result["name"], location: "#{result['venue']['name']}, #{result['venue']['address_1']}, #{result['venue']['city']}", image: "#{image[0]}" }
+      results_set.push(result) if results_set.length < 6
+    end
+
+    bitcoin_meetup = HTTParty.get "https://api.meetup.com/2/events?&sign=true&photo-host=public&fields=self&group_urlname=Refresh-Miami&page=20"
+    bitcoin_meetup["results"].each do |result|
+      targs = result["description"].split("<img src=\"")
+      image = targs[1].split("\" />") rescue ["http://www.andybarratt.co.uk/wp-content/uploads/2013/08/Bitcoin-Coin-1024.jpg"]
+      result = { name: result["name"], location: "#{result['venue']['name']}, #{result['venue']['address_1']}, #{result['venue']['city']}", image: "#{image[0]}" }
+      results_set.push(result) if results_set.length < 6
+    end
+
+    bitcoin_meetup = HTTParty.get "https://api.meetup.com/2/events?&sign=true&photo-host=public&fields=self&group_urlname=Code-for-Miami&page=20"
+    bitcoin_meetup["results"].each do |result|
+      targs = result["description"].split("<img src=\"")
+      image = targs[1].split("\" />") rescue ["http://www.andybarratt.co.uk/wp-content/uploads/2013/08/Bitcoin-Coin-1024.jpg"]
+      result = { name: result["name"], location: "#{result['venue']['name']}, #{result['venue']['address_1']}, #{result['venue']['city']}", image: "#{image[0]}" }
+      results_set.push(result) if results_set.length < 6
+    end
+
+    return results_set
+  end
 
 end
