734a6eaa7789d8fc7167d5a47133946f3aa21c1e
Joshua Echeverri <josheche@gmail.com>
2016-03-26 00:20:46 -0400

    rss parse in app controller for news articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   def init
     @coinbase_price = coinbase_price
     @meetup_event = meetup_event
+    @news = news
   end
 
   def coinbase_price
@@ -49,4 +50,15 @@ class ApplicationController < ActionController::Base
     return results_set
   end
 
+  def news
+    require 'rss'
+    require 'open-uri'
+       rss_results = []
+       rss = RSS::Parser.parse(open('http://feeds.feedburner.com/CoinDesk?format=xml').read, false).items[0..5]
+       rss.each do |result|
+         result = { title: result.title, date: result.pubDate, link: result.link, description: result.description }
+         rss_results.push(result)
+       end
+      return rss_results
+  end
 end
