1fd7509a1e5007e669921e920bde740069524233
Joshua Echeverri <josheche@gmail.com>
2016-03-27 13:41:45 -0400

    leadflet js map updates

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
-  var mymap = L.map('mapid').setView([25.789, -80.226], 11);
+
+  // init the map bro
+  var map = L.map('mapid').setView([25.789, -80.226], 13);
+
+  // load up those tiles bro
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+
     // leaflet docs example
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    attribution: 'Map markers &copy; <a href="https://bitstop.co">Bitstop</a>',
 
     maxZoom: 18,
 
@@ -10,26 +15,5 @@ $(document).ready(function() {
 
     accessToken: 'pk.eyJ1Ijoiam9zaGVjaGUiLCJhIjoiY2lsdGxkNDZ6MDA5dnVza3Ntb2k3MG5ucSJ9.dLVTcxnZoBfQ0LLLKQkdaA',
 
-    // false if you want an unruly map
-    sleep: true,
-
-    // time(ms) until map sleeps on mouseout
-    sleepTime: 300,
-
-    // time(ms) until map wakes on mouseover
-    wakeTime: 750,
-
-    // defines whether the user is prompted on how to wake map
-    sleepNote: true,
-
-    // should hovering wake the map?
-    hoverToWake: true,
-
-    // specify a custom message to notify users how to wake
-    wakeMessage: ('Click ' + 'to find Bitcoin Locations'),
-
-    // opacity (between 0 and 1) of inactive map
-    sleepOpacity: .7,
-
-  }).addTo(mymap);
+  }).addTo(map);
 });
