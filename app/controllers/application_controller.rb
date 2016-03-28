fd7d0b6212a67b3289fb291812db2ac2cbe9148b
Joshua Echeverri <josheche@gmail.com>
2016-03-28 17:20:58 -0400

    lazaro broke my controller again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  require 'rss'
+  require 'open-uri'
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
